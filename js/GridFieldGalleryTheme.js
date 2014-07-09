f352d7433821000becc9a8e4523fc11e0495f929
Thierry François <thierry@colymba.com>
2014-07-09 13:40:04 +0300

    FIX add onunmatch

--- js/GridFieldGalleryTheme.js
+++ js/GridFieldGalleryTheme.js
@@ -8,6 +8,7 @@
           $(this).parents('table.ss-gridfield-table').addClass('galleryTheme');
           $(this).parents('table.ss-gridfield-table.galleryTheme').find('thead tr.sortable-header').empty().remove();
         }
+        onunmatch: function(){}
         
       });
 
