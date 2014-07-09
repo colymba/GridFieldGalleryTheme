bd709a72840039b7edac3e0bbb5fa4b3621726cd
Thierry François <thierry@colymba.com>
2014-07-09 14:25:09 +0300

    missing comma

--- js/GridFieldGalleryTheme.js
+++ js/GridFieldGalleryTheme.js
@@ -7,7 +7,7 @@
         onmatch: function(){
           $(this).parents('table.ss-gridfield-table').addClass('galleryTheme');
           $(this).parents('table.ss-gridfield-table.galleryTheme').find('thead tr.sortable-header').empty().remove();
-        }
+        },
         onunmatch: function(){}
         
       });
